defedcf090535ea3d1831203701af1574c410edd
Anthony Olawo <oolawo09@alaalumni.org>
2015-10-24 07:57:08 -0400

    first commit

---
+++ src/School.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class School {
+	
+	private String schoolName; 
+	private ArrayList<Student> students;
+	private Integer currentID; 
+	
+	School(String schoolName){ 
+		this.schoolName = schoolName; 
+		students = new ArrayList<Student>(); 
+		currentID = 0; 
+	}
+	
+	public void addStudent(String name){ 
+		students.add(new Student(name, currentID)); 
+		currentID++; 
+	}
+	
+	public void payFees(Integer studentId){ 
+		
+	}
+	
+	public Student getStudent(Integer id){ 
+		for(Student student: students){ 
+			if(student.getID() == id){
+				return student; 
+			}
+		}	
+	 return null;
+	}
+	
+}
---
+++ src/storage/write.java
@@ -0,0 +1,8 @@
+package storage;
+
+public class write {
+	
+	public void write(Object obj){ 
+		
+	}
+}
